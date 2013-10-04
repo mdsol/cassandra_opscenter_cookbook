b8b435d9cf4d4c89bac84f74ccb79bf0c2127576
atrullmdsol <atrull@mdsol.com>
2013-10-04 17:59:29 +0200

    Switching back to ec2 public hostname for agent connectivity - this makes sense for ec2 multi-region support.

--- recipes/opscenter-agent.rb
+++ recipes/opscenter-agent.rb
@@ -3,9 +3,9 @@ log "Installing Opscenter Agent"
 # Install Agent
 
 # Download Agent from Leader - the leader may take a while to generate the agent.tar.gz at install time, so we give it up to 40 tries to do it.
-log "Downloading Agent from http://#{$LEADERIPADDRESS}/agent.tar.gz"
-remote_file "#{Chef::Config[:file_cache_path]}/#{$LEADERIPADDRESS}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz" do
-  source "http://#{$LEADERIPADDRESS}/agent.tar.gz"
+log "Downloading Agent from http://#{$LEADEREC2PUBLICHOSTNAME}/agent.tar.gz"
+remote_file "#{Chef::Config[:file_cache_path]}/#{$LEADEREC2PUBLICHOSTNAME}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz" do
+  source "http://#{$LEADEREC2PUBLICHOSTNAME}/agent.tar.gz"
   action :create_if_missing
   mode "0644"
   retries 10
@@ -15,15 +15,15 @@ end
 # Install the Agent according to the Documentation - but clear out the old address.yaml in case there is an update, in case the server changed.
 bash "Opscenter Agent Installation" do
   code <<-EOH
-  cd /tmp/ && tar zxvf #{Chef::Config[:file_cache_path]}/#{$LEADERIPADDRESS}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz && cd agent && ./bin/install_agent.sh opscenter-agent.deb #{$LEADERIPADDRESS}
+  cd /tmp/ && tar zxvf #{Chef::Config[:file_cache_path]}/#{$LEADEREC2PUBLICHOSTNAME}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz && cd agent && ./bin/install_agent.sh opscenter-agent.deb #{$LEADEREC2PUBLICHOSTNAME}
   EOH
-  not_if "dpkg -l opscenter-agent | grep #{node[:cassandra][:opscenter][:version]} && grep #{$LEADERIPADDRESS} /var/lib/opscenter-agent/conf/address.yaml"
+  not_if "dpkg -l opscenter-agent | grep #{node[:cassandra][:opscenter][:version]} && grep #{$LEADEREC2PUBLICHOSTNAME} /var/lib/opscenter-agent/conf/address.yaml"
   notifies :create, "template[/var/lib/opscenter-agent/conf/address.yaml]", :immediately
 end
 
 # Opscenter Agent configuration - we force ssl to be true
 template "/var/lib/opscenter-agent/conf/address.yaml" do
-  variables :LEADERIPADDRESS => $LEADERIPADDRESS
+  variables :LEADEREC2PUBLICHOSTNAME => $LEADEREC2PUBLICHOSTNAME
   source "address.yaml.erb"
   mode      "0644"
   action :nothing
@@ -31,9 +31,9 @@ template "/var/lib/opscenter-agent/conf/address.yaml" do
 end
 
 # Delete the downloaded file if we didn't manage to install it - it is safer to download again and try again
-file "#{Chef::Config[:file_cache_path]}/#{$LEADERIPADDRESS}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz" do
+file "#{Chef::Config[:file_cache_path]}/#{$LEADEREC2PUBLICHOSTNAME}-opscenter-#{node[:cassandra][:opscenter][:version]}-agent.tar.gz" do
   action :delete
-  not_if "dpkg -l opscenter-agent | grep #{node[:cassandra][:opscenter][:version]} && grep #{$LEADERIPADDRESS} /var/lib/opscenter-agent/conf/address.yaml"
+  not_if "dpkg -l opscenter-agent | grep #{node[:cassandra][:opscenter][:version]} && grep #{$LEADEREC2PUBLICHOSTNAME} /var/lib/opscenter-agent/conf/address.yaml"
 end
 
 # We use the opscenter-agent service resource so it must be specified somewhere.

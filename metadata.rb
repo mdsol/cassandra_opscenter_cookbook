5b1c38ae4938d8ccb60d93744897ebab73e51791
atrullmdsol <atrull@mdsol.com>
2013-10-07 22:32:41 +0200

    Better-expressed cookbook dependencies. Bumped cookbook version.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer_email "cookbooks@mdsol.com"
 license          "Apache 2.0"
 description      "Installs Opscenter for monitoring Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.2"
+version "0.0.3"
 
+depends "python"
+depends "java"
 depends "nginx_proxy"

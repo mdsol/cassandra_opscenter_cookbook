cb4ee04c97a942b66eff659f783674dc405e3679
atrullmdsol <atrull@mdsol.com>
2013-10-08 23:16:57 +0200

    Switched from log to Chef::Log.info/Chef::Log.warn Some formatting changes.

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,23 +4,23 @@ peers = search(:node, "roles:#{node[:roles].first}" )
 leader = peers.sort{|a,b| a.name <=> b.name}.first || node # the "or" covers the case where node is the first db
 
 # Some reporting on the election
-log "cassandra-opscenter LeaderElection: #{node[:roles].first} Leader is : #{leader.name} #{leader.ec2.public_hostname} #{leader.ipaddress}"
+Chef::Log.info("cassandra-opscenter LeaderElection: #{node[:roles].first} Leader is : #{leader.name} #{leader.ec2.public_hostname} #{leader.ipaddress}")
 
-# set some global vars to be used in the agent recipe
+# Set some global vars to be used in the agent recipe
 $LEADERNAME = leader.name
 $LEADERIPADDRESS = leader.ipaddress
 $LEADEREC2PUBLICHOSTNAME = leader.ec2.public_hostname
 $LEADERAGENTCHECKSUM = leader.cassandra.opscenter.agent.checksum
 
 if (node.name == leader.name)
-  # leader installs the server - it is the Master
+  # Leader installs the server - it is the Master
   include_recipe "cassandra-opscenter::opscenter-server"
-  # leader installs the agent too
+  # Leader installs the agent too
   include_recipe "cassandra-opscenter::opscenter-agent"
 else 
-  # followers install the agent 
+  # Followers install the agent 
   include_recipe "cassandra-opscenter::opscenter-agent"
-  # and shuts down previous instances of the server - saving memory, etc
+  # And shutdown previous instances of the server - saving memory, etc
   include_recipe "cassandra-opscenter::opscenter-server-shutdown"
 end
 

0446773d35a0cf4771176bc2dcf41e84bd6e1bd8
atrullmdsol <atrull@mdsol.com>
2013-10-08 12:25:52 +0200

    Remove double slash.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default[:cassandra][:opscenter][:home] = "#{node[:cassandra][:parentdir]}/opscen
 
 # What version to install, where to get it and a checksum to guarantee it is valid.
 default[:cassandra][:opscenter][:version] = "3.2.2"
-default[:cassandra][:opscenter][:src_url] = "http://downloads.datastax.com/community//opscenter-#{node['cassandra']['opscenter']['version']}-free.tar.gz"
+default[:cassandra][:opscenter][:src_url] = "http://downloads.datastax.com/community/opscenter-#{node['cassandra']['opscenter']['version']}-free.tar.gz"
 default[:cassandra][:opscenter][:checksum] = "568b9e8767a0ed1bc7f101f39cf400f63fbba4f7dceefafab19c608aaf386950"
 
 # We create fill in this attribute dynamically mid-chef-run on the host that installs the server and generates the package.

165b407514c781b17f1d6b56bbeb2f2c224ac018
atrullmdsol <atrull@mdsol.com>
2013-10-10 15:38:50 +0200

    Applying the Apache 2.0 License

--- recipes/opscenter-server-shutdown.rb
+++ recipes/opscenter-server-shutdown.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: cassandra-opscenter
+# Recipe:: opscenter-server-shutdown
+#
+# Copyright 2013 Medidata Solutions Worldwide
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # We may lose the election one day. In which case we will stop running the Opscenter Server.
 # Metrics are kept in Cassandra itself so this is strangely safe.
 

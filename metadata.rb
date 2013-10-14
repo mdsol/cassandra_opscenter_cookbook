ce113ac8a2c5ea7fdf4fc40e9d78b667ad8a7eec
atrullmdsol <atrull@mdsol.com>
2013-10-14 13:26:47 +0200

    Bumping version and setting the correct name in metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             'cassandra-opscenter'
 maintainer       "Medidata Solutions, Inc."
 maintainer_email "cookbooks@mdsol.com"
 license          "Apache 2.0"
 description      "Installs Opscenter for monitoring Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.4"
+version "0.0.5"
 
 depends "python"
 depends "java"
